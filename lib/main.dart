8fda69b6a1682110898763305867e394b3df2202
karlmacas29 <karlracomamacas@gmail.com>
2024-09-16 13:53:36 +0800

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
       title: 'Baby Sitter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }

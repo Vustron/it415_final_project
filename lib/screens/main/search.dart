f01229c9c2ae4f3cd593a468db2d09d7238e7aa0
Macky Mar Layao <157899268+UzuMacky@users.noreply.github.com>
2024-09-25 13:00:42 +0800

    feat: Search Page UI

--- lib/screens/main/search.dart
+++ lib/screens/main/search.dart
@@ -11,7 +11,25 @@ class _SearchAppState extends State<SearchApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        toolbarHeight: 60,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+        ),
+        centerTitle: false,
+        title: const Text(
+          'Search',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Nexa-Heavy'),
+        ),
+      ),
     );
   }
 }

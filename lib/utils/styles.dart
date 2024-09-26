7f3de392dd60790950fd363ca5692cb11e1acaf3
Vustron Vustronus <runa.michaeljoshua@dnsc.edu.ph>
2024-09-26 19:15:17 +0800

    feat: added headerText style

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,1 +1,11 @@
-mixin GlobalStyles {}
+// utils
+import 'package:flutter/material.dart';
+
+mixin GlobalStyles {
+  /*-------------- Text Styles --------------*/
+  final TextStyle headerText = const TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1,
+  );
+}

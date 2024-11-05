94b0b23ce28335da20d8f3b7340f763448eaa061
Vustron Vustronus <runa.michaeljoshua@dnsc.edu.ph>
2024-11-05 21:35:50 +0800

    feat: added dynamic form

---
+++ lib/models/inputfield.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InputFieldConfig {
+  InputFieldConfig({
+    required this.label,
+    required this.type,
+    this.value,
+    this.keyboardType,
+    this.obscureText = false,
+    this.hintText = 'Enter text...',
+    this.prefixIcon,
+    this.cursorColor,
+    this.textInputAction,
+  });
+
+  final String label;
+  final String type;
+  final String? value;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final String hintText;
+  final IconData? prefixIcon;
+  final Color? cursorColor;
+  final TextInputAction? textInputAction;
+}

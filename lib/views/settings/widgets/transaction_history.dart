ae8527cf7282a72b251a93c11ed67cde17a19f5d
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-06 15:57:03 +0800

    feat: improved transaction history layout

--- lib/views/settings/widgets/transaction_history.dart
+++ lib/views/settings/widgets/transaction_history.dart
@@ -19,61 +19,156 @@ class TransactionHistory extends StatelessWidget {
         children: <Widget>[
           const SizedBox(height: 20),
 
+          const SizedBox(width: 14),
+
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
 
-              PopupMenuButton<String>(
-                tooltip: '',
-                onSelected: (String result) {
-                  if (result == 'Payment') {
-                    // Do something for Payment
-                  } else if (result == 'Service Profile') {
-                    // Do something for Service Profile
-                    
-                  }
-                },
-                itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                  const PopupMenuItem<String>(
-                    value: 'Payment',
-                    child: Text('Payment'),
-                  ),
+              ElevatedButton(
+                onPressed: (){}, 
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  )
+                ),
 
-                  const PopupMenuItem<String>(
-                    value: 'Service Profile',
-                    child: Text('Service Profile'),
-                  ),
-
-                ],
-                
-
-                child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        'Filter Transaction',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                      SizedBox(width: 2),
-                      Icon(Icons.filter_alt_rounded, size: 30.0),
-
-                    ],
-                  ),
+                child: const Text('Gcash'),
               ),
 
+              ElevatedButton(
+                onPressed: (){}, 
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  )
+                ),
 
-              const SizedBox(width: 14),
+                child: const Text('Cash on Service'),
+              ),
+
+              Column(
+                children: <Widget> [
+                  PopupMenuButton<String>(
+                    tooltip: '',
+                    onSelected: (String result) {
+                      if (result == 'Payment') {
+                        // Do something for Payment
+                      } else if (result == 'Service Profile') {
+                        // Do something for Service Profile
+                        
+                      }
+                    },
+                    itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                      const PopupMenuItem<String>(
+                        value: 'Any time',
+                        child: Text('Any time'),
+                      ),
+                  
+                      const PopupMenuItem<String>(
+                        value: 'Older than a week',
+                        child: Text('Older than a week'),
+                      ),
+
+                      const PopupMenuItem<String>(
+                        value: 'Older than a month',
+                        child: Text('Older than a month'),
+                      ),
+
+                      const PopupMenuItem<String>(
+                        value: 'Older than 6 months',
+                        child: Text('Older than 6 months'),
+                      ),
+
+                      const PopupMenuItem<String>(
+                        value: 'Older than a year',
+                        child: Text('Older than a year'),
+                      ),
+                  
+                    ],
+                    
+                  
+                    child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Filter',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          SizedBox(width: 2),
+                          Icon(Icons.filter_alt_rounded, size: 30.0),
+                  
+                        ],
+                      ),
+                  ),
+
+                  const SizedBox(height: 10,),
+
+                  
+                ],
+              ),
 
               
             ],
           ),
 
+
+
+
+
+          Row(
+            children: <Widget>[
+              const SizedBox(height: 10,),
+              PopupMenuButton<String>(
+                        tooltip: '',
+                        onSelected: (String result) {
+                          if (result == 'Payment') {
+                            // Do something for Payment
+                          } else if (result == 'Service Profile') {
+                            // Do something for Service Profile
+                            
+                          }
+                        },
+                        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                          const PopupMenuItem<String>(
+                            value: 'All',
+                            child: Text('All'),
+                          ),
+                      
+                          const PopupMenuItem<String>(
+                            value: 'Recent',
+                            child: Text('Recent'),
+                          ),
+                      
+                        ],
+                        
+                      
+                        child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              
+                              SizedBox(width: 15),
+                              Text(
+                                'Recent',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Icon(Icons.arrow_drop_down, size: 40.0),
+                      
+                            ],
+                          ),
+                      ),
+            ],
+          ),
+
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(4.0),
               children: const <Widget>[
                 TransactionCard(
                   date: 'Sep 22, 2024',

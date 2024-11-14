eb999c5389ce3a1a8edb9238e51ee1aac2e733fb
karlracomamacas@gmail.com <karlracomamacas@gmail.com>
2024-11-14 15:57:56 +0800

    modified:   lib/core/constants/views_list.dart 	modified:   lib/views/(home_Macas_Millan)/home/view.dart 	modified:   lib/views/(home_Macas_Millan)/home/widgets/card_nearby.dart 	modified:   lib/views/(home_Macas_Millan)/home/widgets/toprate_card.dart 	modified:   lib/views/(home_Macas_Millan)/home_babysitter/views.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/card_page.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/comment_list.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/graph_page.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/header_page.dart 	deleted:    lib/views/(home_Macas_Millan)/home_babysitter/widgets/navigation.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/photo_row.dart 	new file:   lib/views/(home_Macas_Millan)/home_babysitter/widgets/title_header.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/core/constants/views_list.dart
+++ lib/core/constants/views_list.dart
@@ -1,3 +1,5 @@
+// import 'package:babysitterapp/controllers/auth_controller.dart';
+// import 'package:babysitterapp/core/state/authentication_state.dart';
 import 'package:flutter/material.dart';
 
 import 'package:babysitterapp/views/(settings_JK_Gerald)/account/view.dart';
@@ -5,6 +7,7 @@ import 'package:babysitterapp/views/(home_Macas_Millan)/notification_client/view
 import 'package:babysitterapp/views/(home_Macas_Millan)/message/view.dart';
 import 'package:babysitterapp/views/(home_Macas_Millan)/home/view.dart';
 
+//required Hot reload to see changes
 final List<Widget> screens = <Widget>[
   HomeView(),
   MessageView(),
